d110002142b90d1edb13bf6b9aef09dcad00aa00
Rebeca Pérez <161166821+Pro-gra-mer@users.noreply.github.com>
2024-10-23 18:49:53 +0200

    Sección últimos artículos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardsComponent } from '../../components/cards/cards.component';
+import { ArticlesComponent } from '../../components/articles/articles.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardsComponent],
+  imports: [CardsComponent, ArticlesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
