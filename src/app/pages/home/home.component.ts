825b47b4555107e5e8050ab7a264fcd92b51b39c
Rebeca Pérez <161166821+Pro-gra-mer@users.noreply.github.com>
2024-10-24 16:53:42 +0200

    Idea visual terminada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardsComponent } from '../../components/cards/cards.component';
 import { ArticlesComponent } from '../../components/articles/articles.component';
+import { ProyectsComponent } from '../../components/proyects/proyects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardsComponent, ArticlesComponent],
+  imports: [CardsComponent, ArticlesComponent, ProyectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
